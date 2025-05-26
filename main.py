b602891ed75aa7dfdde7704c9f160ec52ba08e44
vinex <notrealmailfornow@proton.me>
2025-05-26 19:01:17 -0300

    Add ChatAnthropic integration and update dependencies for MCP tools

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import asyncio
 from dotenv import load_dotenv
 import os
 
+from mcp import ClientSession, StdioServerParameters
+from langchain_anthropic import ChatAnthropic
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
 load_dotenv()
-print(os.getenv("ANTHROPIC_API_KEY"))
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-20240620", anthropic_api_key=os.getenv("ANTHROPIC_API_KEY"))
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args = ["/Users/vicious/Desktop/bcd/servers/mathserver.py"],
+)
+
+
 
 async def main():
     print("Hello from bcd!")
-    print(os.getenv("OPENAI_API_KEY"))
 
 if __name__ == "__main__":
     asyncio.run(main())
